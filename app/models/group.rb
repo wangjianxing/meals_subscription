347691ce866dbe4ae40acf9050f3fb9a14efe307
wangjianxing <wjx0825@gmail.com>
2020-07-26 23:52:59 +0800

    [UPDATE]group orders comment

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   belongs_to :cutoff
   has_many :orders
 
+  #cutoff created orders + new subscriptions created orders
   def group_orders
     orders.or(cutoff.orders.where(created_at: 1.week.ago(completed_at)..completed_at))
   end
