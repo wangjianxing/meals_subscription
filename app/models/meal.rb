fdf24ac2d33d76b219cafd609e3b179b0473e301
wangjianxing <wjx0825@gmail.com>
2020-07-26 23:44:05 +0800

    [ADD]popular meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,4 +5,11 @@ class Meal < ApplicationRecord
 
   validates_uniqueness_of :name, :sku
   validates_presence_of :name, :description, :sku, :price
+
+  # sort by SubscriptionsMeal's meal count
+  def self.popular_meals
+    meals_count = SubscriptionsMeal.where(subscription_id: Subscription.active.ids).group(:meal_id).count
+    sorted_by_count_meal_ids = meals_count.sort { |a, b| b[1] <=> a[1] }.map { |mc| mc[0] }
+    Meal.where(id: sorted_by_count_meal_ids.first(10))
+  end
 end
